83d8cd00ad6ddd95920419dfcfe7171bbd5179e1
DavidMcLaughlin55 <49570547+DavidMcLaughlin55@users.noreply.github.com>
2021-10-12 15:14:12 -0500

    Created Quote Array

--- js/script.js
+++ js/script.js
@@ -11,12 +11,39 @@ Project 4 - Random Quote Generator
  * `quotes` array 
 ***/
 
+const quotes = [
+  {quote:'The truth is rarely pure and never simple.', 
+  source: 'Oscar Wilde',
+  citation: 'The Importance of Being Earnest',
+  year: 1895},
+  {quote:'You can have tons of talent, but it won\'t necessarily keep you fed. If you have sharp instincts, through, you\'ll never go hungry.', 
+  source: 'Haruki Murakami',
+  citation: '1Q84',
+  year: 2009},
+  {quote:'It\'s better to have something to remember than anything to regret.', 
+  source: 'Frank Zappa',
+  citation: 'The Real Frank Zappa Book',
+  year: 1990},
+  {quote:'There is no greater agony than bearing an untold story inside you.', 
+  source: 'Maya Angelou',
+  citation: 'I Know Why The Caged Bird Sings',
+  year: 1969},
+  {quote:'Fairy tales are more than true: not because they tell us that dragons exist, but because they tell us that dragons can be beaten.', 
+  source: 'Neil Gaiman',
+  citation: 'Coraline',
+  year: 2002},
+];
+
+//console.log(quotes);
+
 
 
 /***
  * `getRandomQuote` function
 ***/
-
+function getRandomQuote() {
+  
+}
 
 
 /***
